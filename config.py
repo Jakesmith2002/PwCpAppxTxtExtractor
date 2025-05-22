9251f01b83a18addbaa60067871c0746a9da5cbb
Jakesmith2002 <148589301+Jakesmith2002@users.noreply.github.com>
2025-05-22 23:18:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 api_id = "24473318"
 api_hash = "e7dd0576c5ac0ff8f90971d6bb04c8f5"
-bot_token = "8034069514:AAHUBpzSCq41jPwsJkDbXuEoVC_yCxzyuw0"
-auth_users = [5840594311]
+bot_token = "7925762414:AAGroRQEHt487537u9IOE4hKHskK3oX9MM4"
+auth_users = [1358747363]
